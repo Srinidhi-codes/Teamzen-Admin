614b9da6f0fc799817ced70a33c9df77702a88c4
Srinidhi-codes <66681637+Srinidhi-codes@users.noreply.github.com>
2026-01-27 23:25:13 +0530

    feat: Add GraphQL hooks for user queries and mutations.

--- lib/graphql/users/userHook.ts
+++ lib/graphql/users/userHook.ts
@@ -1,6 +1,7 @@
-import { useQuery } from "@apollo/client/react"
+import { useQuery, useMutation } from "@apollo/client/react"
 import { User } from "./types"
 import { GET_ALL_USERS } from "./queries"
+import { CREATE_USER, UPDATE_USER } from "./mutations"
 
 export function useGraphQLUsers(){
 
@@ -13,5 +14,25 @@ export function useGraphQLUsers(){
         isUsersLoading: loading,
         isUsersError: error,
         refetchUsers: refetch
+    }
+}
+
+export function useGraphQLUserMutations() {
+    const [createUser, { loading: isCreatingUser, error: createUserError }] = useMutation<any>(CREATE_USER);
+    const [updateUser, { loading: isUpdatingUser, error: updateUserError }] = useMutation<any>(UPDATE_USER);
+
+    return {
+        createUser: async (input: any) => {
+             const response = await createUser({ variables: { input } });
+             return response.data?.createUser;
+        },
+        updateUser: async (userId: string, input: any) => {
+            const response = await updateUser({ variables: { userId, input } });
+            return response.data?.updateUser;
+        },
+        isCreatingUser,
+        createUserError,
+        isUpdatingUser,
+        updateUserError
     }
 }
