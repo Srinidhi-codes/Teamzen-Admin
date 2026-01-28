bd295a052ecdb24b12175802f7192783dea72cc6
Srinidhi-codes <66681637+Srinidhi-codes@users.noreply.github.com>
2026-01-28 23:38:36 +0530

    feat: add attendance correction approval workflow and components

---
+++ lib/graphql/attendance/mutations.ts
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+export const CHECK_IN = gql`
+  mutation CheckIn($input: CheckInInput!) {
+    checkIn(input: $input) {
+      id
+      loginTime
+      status
+    }
+  }
+`;
+
+export const CHECK_OUT = gql`
+  mutation CheckOut($input: CheckOutInput!) {
+    checkOut(input: $input) {
+      id
+      logoutTime
+      status
+    }
+  }
+`;
+
+export const REQUEST_ATTENDANCE_CORRECTION = gql`
+  mutation RequestAttendanceCorrection($input: AttendanceCorrectionInput!) {
+    requestAttendanceCorrection(input: $input) {
+      id
+      status
+    }
+  }
+`;
+
+export const CANCEL_ATTENDANCE_CORRECTION = gql`
+  mutation CancelAttendanceCorrection($correctionId: ID!) {
+    cancelAttendanceCorrection(correctionId: $correctionId) {
+      id
+      status
+    }
+  }
+`;
+
+export const APPROVE_OR_REJECT_ATTENDANCE_CORRECTION = gql`
+  mutation ApproveOrRejectAttendanceCorrection($input: ApproveAttendanceCorrectionInput!) {
+    approveOrRejectAttendanceCorrection(input: $input)
+  }
+`;
